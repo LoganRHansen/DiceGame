a7b1ffa872d660f99484c32e31babbd051c4891f
Logan <lrh1443@gmail.com>
2017-12-07 14:25:53 -0600

    first commit

---
+++ dicegame.js
@@ -0,0 +1,10 @@
+"use strict";
+
+function rolldice() {
+	 let a = Math.floor(Math.random() * (4) + 1)
+	let  b = Math.floor(Math.random() * (6) + 1)
+	let  c = Math.floor(Math.random() * (8) + 1)
+	let  d = Math.floor(Math.random() * (10) + 1)
+	let  e = Math.floor(Math.random() * (12) + 1)
+	let  f = Math.floor(Math.random() * (20) + 1)
+}
